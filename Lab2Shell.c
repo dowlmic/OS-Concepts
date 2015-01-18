69b7dbfd350ed12949ddfac699cc00b1d1407ac8
vagrant <vagrant@precise32.(none)>
2015-01-18 22:53:00 +0000

    Added functionality to read user input, then convert the input to array of strings for execvp consumption. All that is left is to monitor performance.

--- Lab2Shell.c
+++ Lab2Shell.c
@@ -3,41 +3,47 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 int main()
-{ 
+{
 
     // use these variables
-
     pid_t pid, child;
     int status;
-    
+    char input[30];
 
     while(1)
     {
-	char command[80];
-	printf("Command> ");
-	scanf("%s", command);
+        memset(input, '\0', sizeof input); // Reset Input String
 
-	if(strcmp("quit", command) == 0) // Exit on Quit
-		exit(0);
-
-	char * prog = strtok(command, " "); // Gets the program
-	//printf("%s", command);
-	printf("%s", prog);
+        fgets(input, sizeof input, stdin);
+        if(strcmp("quit\n", input) == 0) // Exit on Quit
+            exit(0);
 
         if ((pid = fork()) < 0) { // Creates the Child
             perror("fork failure");
             exit(1);
         }
         else if (pid == 0) { // If Child
-//            execvp();
-   	    exit(0);
-
+            char* param_array[50] = {NULL}; // Initialize array to null
+            char * token = strtok(input, " \n"); // get token
+            int index = 0;
+            while (token != NULL) // Convert input into array of strings
+            {
+                param_array[index] = malloc(strlen(token));
+                param_array[index] = token;
+                token = strtok(NULL, " \n");
+                index++;
+            }
+            param_array[index] = NULL; // End the Array
+            execvp(param_array[0], param_array); // Execute the program
+            memset(param_array, '\0', sizeof param_array); // Reset the memory for the array
+            exit(0);
         }
         else {
-//	    wait(&status);
+            child = wait(&status);
         }
     }
     return 0;
-} 
+}
