51739ee70cd8ae3cdb9e40a37b65347e6e529b52
dowlmic <dowlmic@gmail.com>
2015-01-29 17:07:37 -0500

    All but signal handling implemented for program

--- lab4/FSEmu.c
+++ lab4/FSEmu.c
@@ -4,18 +4,48 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
 
 
 void* find_file(void* arg);
 
 int main(){
-    char* input;
+    pthread_t thread;
+    int status;
+    char input[50];
+    int numRequests = 0;
+
+    // Set up randomness
+    srand(time(NULL));
+
+    // Continuously receive file requests
     while(1){
         fgets(input, 50, stdin);
-	find_file(input);
+	if ((status = pthread_create(&thread, NULL, find_file, input)) != 0) {
+	    fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
+	    exit(1);
+	}
+	if ((status = pthread_detach(thread)) != 0) {
+	    fprintf(stderr, "detach error %d: %s\n", status, strerror(status));
+	}
+        numRequests++;
     }
+    exit(0);
 }
 
 void* find_file(void* arg){
-  
+    // Simulate a chached file vs a non-cached file
+    int chachedFile = rand() % 5;
+    if (chachedFile <= 3) {
+	sleep(1);
+    }
+    else {
+	int sleepTime = (rand() % 3) + 7;
+        sleep(sleepTime);
+    }
+
+    // Simulate file retrieval
+    printf("Retreived file: %s\n", (char *)arg);  
+
+    return NULL;
 }
