a495bb7548d5a0ab8d5f132aac415f2a3e1d1805
Ordaz <ordazl@eos24.cis.gvsu.edu>
2015-02-05 17:39:10 -0500

    Copied of writer code to reader.

--- lab5/reader.c
+++ lab5/reader.c
@@ -4,9 +4,44 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+#include <string.h>
+#include "sh_defs.h"
 
 int main(){
+   int shmId;
+   int *shmPtr;
 
-    return 0;
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
+      perror ("Could not attach to new shared memory space\n");
+      exit (1);
+   }
+
+   struct sh_mem *mem = (void *)shmPtr;
+   mem->writeable = 1;
+   memset(mem->read_array, 0, 2);
+   memset(mem->message, '\0', 25);
+
+   while(strcmp(mem->message, "quit\n") != 0)
+   {
+       if(mem->writeable == 1){
+           fgets(mem->message, 25, stdin);
+           mem->writeable = 0;
+       }
+       
+   }
+
+
+   if (shmdt (shmPtr) < 0) {
+
+      perror ("Can't detach from shared memory\n");
+      exit (1);
+   }
+
+   if (shmctl (shmId, IPC_RMID, 0) < 0) {
+      perror ("Can't stuff\n");
+      exit(1);
+   } 
+   return 0;
 }
+
+
